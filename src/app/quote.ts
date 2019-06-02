597bd03ac34009475d21da85d16ad20ecc0f21b0
vincent <ododovincent54@gmail.com>
2019-06-02 20:19:37 +0300

    added complete Date pipe

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
   public showDescription: boolean;
-  constructor(public id: number, public name:string,public description:string,public vote:number){
+  constructor(public id: number, public name:string,public description:string,public vote:number,public completeDate:Date){
     this.showDescription = false;
   }
 }
