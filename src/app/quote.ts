ae713261879dde1813788eef6284aee5fb6bb7f3
vincent <ododovincent54@gmail.com>
2019-06-02 17:58:37 +0300

    got my delete quote button to work

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
   public showDescription: boolean;
-  constructor(public id: number, public name:string,public description:string,public upvotes:number, public downvotes:number){
+  constructor(public id: number, public name:string,public description:string,public vote:number){
     this.showDescription = false;
   }
 }
