09eb8f800db68f379a27a70b12c5a47ce4a723cc
vincent <ododovincent54@gmail.com>
2019-06-02 20:41:01 +0300

    linked my date count pipe to component html

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -5,8 +5,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
-  }
+  transform(value: any):number{
+    let today:Date = new Date();//Get the current date
+    let todayWithNoTime:any = new Date(today);
+    var dateDifference = todayWithNoTime-value;
+    const secondsInDay = 86400;
 
+    var dateDifferenceSeconds = dateDifference*0.001;
+
+    var dateCounter = dateDifferenceSeconds/secondsInDay;
+    console.log(value);
+
+    if(dateCounter>=1) return Math.round(dateCounter);
+    else return 0;
+  }
 }
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
   public showDescription: boolean;
-  constructor(public id: number, public name:string,public description:string,public vote:number,public completeDate:Date){
+  constructor(public id: number, public name:string,public description:string,public vote:number,public date:Date){
     this.showDescription = false;
   }
 }
