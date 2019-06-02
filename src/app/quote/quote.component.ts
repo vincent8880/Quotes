76258dfb1e0b75e710b208073214bfc36db989aa
vincent <ododovincent54@gmail.com>
2019-06-02 18:18:11 +0300

    added the upvote and downvote button

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,12 +10,12 @@ import {Quote} from '../quote'
 })
 export class QuoteComponent implements OnInit {
   quotes = [
-    new Quote(1, 'Give light and people will find the way', 'Ella Baker',0,0),
-    new Quote(2, 'One could not pluck a flower without troubling a star', 'Loren Eiseley',0,0),
-    new Quote(3, 'Courage is fear holding on a minute longer', 'George S. Patton',0,0),
-    new Quote(4, 'A cure for seasickness is to sit under a tree', 'Spike Milligan',0,0),
-    new Quote(5, 'Hope is independent of the apparatus of logic', 'Norman Cousins',0,0),
-    new Quote(6, 'Reach for it, push yourself as far as you can', 'Christa McAullife',0,0),
+    new Quote(1, 'Give light and people will find the way', 'Ella Baker',0,),
+    new Quote(2, 'One could not pluck a flower without troubling a star', 'Loren Eiseley',0,),
+    new Quote(3, 'Courage is fear holding on a minute longer', 'George S. Patton',0,),
+    new Quote(4, 'A cure for seasickness is to sit under a tree', 'Spike Milligan',0,),
+    new Quote(5, 'Hope is independent of the apparatus of logic', 'Norman Cousins',0,),
+    new Quote(6, 'Reach for it, push yourself as far as you can', 'Christa McAullife',0,),
 
 ];
 toogleDetails(index){
@@ -29,6 +29,12 @@ deleteGoal(isComplete,index){
           this.quotes.splice(index,1)
       }
   }
+}
+addVotes(index){
+  this.quotes[index].vote += 1
+}
+minusVotes(index){
+  this.quotes[index].vote -=1
 }
 
   constructor() { }
