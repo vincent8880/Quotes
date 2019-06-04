60bd5f880a38d1c4caffe4656539b7219ab704ec
root <ododovincent54@gmail.com>
2019-06-04 13:25:40 +0300

    Auto-generated commit

--- src/app/quote.ts
+++
@@ -1,8 +0,0 @@
-export class Quote {
-  public showDescription: boolean;
-  constructor(public id: number, public name:string,public description:string,public vote:number,public date:Date,public maxCount:number=0)
-  {
-    this.showDescription = false;
-    this.vote = 0;
-  }
-}
